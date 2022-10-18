fe02bc7d7b0642e5f1e97384ae3ee3af47bc901e
bruno <brunoramos013@gmail.com>
2022-10-17 21:36:13 -0300

    Co-authored-by: phcom <phcom@users.noreply.github.com> Co-authored-by: André Luiz Rodrigues <Andre-LR@users.noreply.github.com>

--- src/main/java/com/trabalho1/CtrlEspacoAereo.java
+++ src/main/java/com/trabalho1/CtrlEspacoAereo.java
@@ -2,8 +2,12 @@ package com.trabalho1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "com.trabalho1" })
+@EntityScan(basePackages = { "com.trabalho1" })
 public class CtrlEspacoAereo {
 
 	public static void main(String[] args) {
---
+++ src/main/resources/db/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO aeronaves (prefixo,autonomia,velocidadeDeCruzeiro) VALUES ('POA-run',35,17);
+INSERT INTO aeronaves (prefixo,autonomia,velocidadeDeCruzeiro) VALUES ('POA-www',32,43);
+INSERT INTO aeronaves (prefixo,autonomia,velocidadeDeCruzeiro) VALUES ('Win-Run',28,3);
+INSERT INTO aeronaves (prefixo,autonomia,velocidadeDeCruzeiro) VALUES ('Spr-hun',29,32);
+INSERT INTO aeronaves (prefixo,autonomia,velocidadeDeCruzeiro) VALUES ('Sum-dun',26,18);
